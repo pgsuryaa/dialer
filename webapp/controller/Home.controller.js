909a47c17ab8de4163f3c1721748ecc8505955b8
pgsuryaa500987 <pgsuryaa111@gmail.com>
2018-10-28 16:17:41 +0400

    c

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("sap.ui5.dialerzDialer.controller.Home", {
+		onInit: function() {
+			this.oModel = new JSONModel({
+				Number: ""
+			});
+			this.getView().setModel(this.oModel);
+			this.oModel.refresh();
+		},
+		onNumberChange: function(oEvent) {
+			this.oModel.getData().Number += oEvent.getSource().getText();
+			this.oModel.refresh();
+		}
+	});
+});
